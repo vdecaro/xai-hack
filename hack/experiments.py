618f4578062de6257b0c4023b1e4990a3328293e
vdecaro <43961074+vdecaro@users.noreply.github.com>
2024-07-05 13:32:52 +0200

    Lunch commit

--- hack/experiments.py
+++ hack/experiments.py
@@ -1,1 +1,77 @@
+# General
+import pandas as pd
+import numpy as np
+import sklearn
+import shap
+
+# Preprocessing
+from itertools import cycle
+from sklearn import feature_selection
+from sklearn import model_selection
+from sklearn import metrics
+from scipy import stats
+from sklearn.model_selection import train_test_split, KFold, cross_val_predict
+from sklearn.feature_selection import RFECV, RFE
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import SMOTE
+
+# Models
+from sklearn.preprocessing import (
+    StandardScaler,
+    MinMaxScaler,
+    OneHotEncoder,
+    LabelEncoder,
+    label_binarize,
+)
+from sklearn import (
+    svm,
+    tree,
+    linear_model,
+    neighbors,
+    naive_bayes,
+    ensemble,
+    discriminant_analysis,
+    gaussian_process,
+)
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier, plot_importance
+import xgboost as xgb
+from lightgbm import LGBMClassifier
+from sklearn.cluster import KMeans
+
+# Model_Evaluation
+from sklearn.metrics import (
+    roc_curve,
+    auc,
+    make_scorer,
+    accuracy_score,
+    precision_score,
+    recall_score,
+)
+from sklearn.metrics import f1_score, classification_report, average_precision_score
+
+# from sklearn.metrics import plot_confusion_matrix,
+from sklearn.metrics import roc_auc_score, precision_recall_curve
+
+# Visualization
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+from matplotlib.colors import LinearSegmentedColormap
+import seaborn as sns
+
+# Setting_Parameters
+import warnings
+
+warnings.filterwarnings("ignore")
+sns.set()
+style.use("ggplot")
+pd.set_option("display.max_rows", 100)
+
+
+plt.rcParams["figure.figsize"] = (12, 8)
+cmap = LinearSegmentedColormap.from_list(
+    "", ["#FFFFFF", "#FFF5BD", "#FF4646", "#E41A1C", "#960018"]
+)
+DATA_PATH = "/home/decaro/xai-hack/data/credit_card_churn.csv"
+
 from ray import tune
---
+++ hack/utils.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+
+
+DATA_PATH = "/home/decaro/xai-hack/data/credit_card_churn.csv"
+
+
+def load_data():
+    """Load the data from the csv file."""
+    df = pd.read_csv(DATA_PATH)
+    df.drop(
+        [
+            "Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1",
+            "Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2",
+        ],
+        axis=1,
+        inplace=True,
+    )
+    reduce_mem_usage(df)
+
+    # Label Encoding
+    le_Education_Level = LabelEncoder()
+    le_Gender = LabelEncoder()
+    le_Income_Category = LabelEncoder()
+    le_Card_Category = LabelEncoder()
+    le_Marital_Status = LabelEncoder()
+
+    df["Education_Level_n"] = le_Education_Level.fit_transform(df["Education_Level"])
+    df["Income_Category_n"] = le_Income_Category.fit_transform(df["Income_Category"])
+    df["Card_Category_n"] = le_Card_Category.fit_transform(df["Card_Category"])
+    df["Gender_n"] = le_Gender.fit_transform(df["Gender"])
+    df["Marital_Status_n"] = le_Marital_Status.fit_transform(df["Marital_Status"])
+
+    df = df.drop(
+        [
+            "Education_Level",
+            "Income_Category",
+            "Card_Category",
+            "CLIENTNUM",
+            "Gender",
+            "Marital_Status",
+        ],
+        axis=1,
+    )
+    df["Attrition_Flag"] = df["Attrition_Flag"].map(
+        {"Existing Customer": 1, "Attrited Customer": 0}
+    )
+    cols1 = []
+    cols2 = []
+    value = []
+    matrix = df.corr()
+    for i in range(len(matrix.columns)):
+        for j in range(i):
+            cols1.append(matrix.columns[i])
+            cols2.append(matrix.columns[j])
+            value.append(matrix.iloc[i, j])
+    new_df = pd.DataFrame(
+        {"Feature Name 1": cols1, "Feature Name 2": cols2, "Correlation": value}
+    )
+    new_df.sort_values("Correlation", ascending=False, inplace=True)
+    new_df.reset_index(drop=True, inplace=True)
+    new_df = new_df[new_df["Correlation"] > 0.85]
+    new_df["Feature Name 1"].unique()
+    cols1 = []
+    matrix = df.corr()
+    for i in range(len(matrix.columns)):
+        for j in range(i):
+            if abs(matrix.iloc[i, j]) >= 0.85:
+                cols1.append(matrix.columns[i])
+    dropped = df.drop(cols1, axis=1)
+    y_data = dropped["Attrition_Flag"]
+    x_data = dropped[dropped.columns.difference(["Attrition_Flag"])]
+    x_cols = x_data.columns
+    scaler = StandardScaler()
+    x_data = scaler.fit_transform(x_data)
+    x_data = pd.DataFrame(x_data, columns=x_cols)
+
+
+def reduce_mem_usage(train_data: pd.DataFrame):
+    """iterate through all the columns of a dataframe and modify the data type
+    to reduce memory usage.
+    """
+    start_mem = train_data.memory_usage().sum() / 1024**2
+    print("Memory usage of dataframe is {:.2f} MB".format(start_mem))
+
+    for col in train_data.columns:
+        col_type = train_data[col].dtype
+
+        if col_type != object:
+            c_min = train_data[col].min()
+            c_max = train_data[col].max()
+            if str(col_type)[:3] == "int":
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    train_data[col] = train_data[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    train_data[col] = train_data[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    train_data[col] = train_data[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    train_data[col] = train_data[col].astype(np.int64)
+            else:
+                if (
+                    c_min > np.finfo(np.float16).min
+                    and c_max < np.finfo(np.float16).max
+                ):
+                    train_data[col] = train_data[col].astype(np.float16)
+                elif (
+                    c_min > np.finfo(np.float32).min
+                    and c_max < np.finfo(np.float32).max
+                ):
+                    train_data[col] = train_data[col].astype(np.float32)
+                else:
+                    train_data[col] = train_data[col].astype(np.float64)
+        else:
+            train_data[col] = train_data[col].astype("category")
+
+    end_mem = train_data.memory_usage().sum() / 1024**2
+    print("Memory usage after optimization is: {:.2f} MB".format(end_mem))
+    print("Decreased by {:.1f}%".format(100 * (start_mem - end_mem) / start_mem))
+
+    return train_data
